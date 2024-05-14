db745a0d492baad1686feff4c1aece4778c16b9e
Maxime <maxime.b2494@gmail.com>
2024-05-14 14:58:39 +0200

    first commit

---
+++ src/main/java/com/doranco/cda/entity/User.java
@@ -0,0 +1,4 @@
+package com.doranco.cda.entity;
+
+public class User {
+}
---
+++ src/test/java/com/doranco/cda/CdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doranco.cda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
